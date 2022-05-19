9e36f3285f202084a59263b4f670502ba01e0f43
Yazid Escudero <yazidescudero0414@gmail.com>
2022-05-18 21:00:54 -0600

    Add ArrayList attributes, their initialization routines, and method verifyFileSystem()

--- src/bsuclasses/files/FileManager.java
+++ src/bsuclasses/files/FileManager.java
@@ -1,5 +1,6 @@
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 
@@ -21,14 +22,63 @@ public class FileManager {
   private File directoryExpenses;
   private File fileCompleteExpenses;
 
+  private ArrayList<File> filesInBudgets;
+  private ArrayList<File> filesInExpenses;
+
   // INITIALIZATION BLOCK /////
   {
     this.directoryBSUDatabase = new File("./BSUDatabase/");
     this.directoryBudgets = new File(this.directoryBSUDatabase, "budgets/");
     this.directoryExpenses = new File(this.directoryBSUDatabase, "expenses/");
     this.fileCompleteExpenses = new File(this.directoryExpenses, "completeexpenses.txt");
+
+    this.filesInBudgets = new ArrayList<File>();
+    this.filesInExpenses = new ArrayList<File>();
+
+    // Populate this.filesInBudgets
+    {
+      File[] files = (this.directoryBudgets).listFiles();
+      int numberOfFiles = files.length;
+      for(int index = 0; index < numberOfFiles; ++index)
+        (this.filesInBudgets).add(files[index]);
+    }
+
+    // Populate this.filesInExpenses
+    {
+      File[] files = (this.filesInExpenses).listFiles();
+      int numberOfFiles = files.length;
+      for(int index = 0; index < numberOfFiles; ++index) {
+        String currentFileName = files[index];
+        if(currentFileName.equals("completeexpenses.txt"))
+          continue;
+
+        (this.filesInExpenses).add(files[index]);
+      }
+
+    }
   }
 
-  
+  /**
+   Makes sure that FileSystem directories and files necessary for program operations
+   exist - creates them if something is missing and doesn't exist
+
+   @throws IOExeption
+           when something goes wrong creating necessary program files
+  */
+  public void verifyFileSystem() throws IOException{
+    // First verify directories
+    if(!((this.directoryBSUDatabase).exists()))
+      (this.directoryBSUDatabase).mkdir();
+
+    if(!((this.directoryBudgets).exists()))
+      (this.directoryBudgets).mkdir();
+
+    if(!((this.directoryExpenses).exists()))
+      (this.directoryExpenses).mkdir();
+
+    // Then verify files
+    if(!((this.fileCompleteExpenses).exists()))
+      (this.fileCompleteExpenses).createNewFile();
+  }
 
 }
