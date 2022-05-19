1673b5096e1bf1c54d937b51d58dbfcbf5249a6f
Yazid Escudero <yazidescudero0414@gmail.com>
2022-05-19 00:23:29 -0600

    Add class Budget

---
+++ src/bsuclasses/budget/Budget.java
@@ -0,0 +1,15 @@
+
+package bsuclasses.budget;
+
+/**
+ A {@code Budget} object will represent a budget for a month as specified by the user.
+
+ @author Yazid Escudero
+ @version 1.0.0
+ @since 1.0.0
+*/
+public class Budget {
+
+
+
+}
