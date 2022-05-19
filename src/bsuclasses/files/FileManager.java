d9fadbcd90f858378ff1e200a2cb0da95674ef59
Yazid Escudero <yazidescudero0414@gmail.com>
2022-05-18 20:28:40 -0600

    Add attributes & initilization block

--- src/bsuclasses/files/FileManager.java
+++ src/bsuclasses/files/FileManager.java
@@ -1,4 +1,7 @@
 
+import java.io.File;
+import java.util.ArrayList;
+
 
 /**
  A FileManager object manages the program's files and folders, and their overall
@@ -9,6 +12,23 @@
 */
 public class FileManager {
 
+  // CLASS ATTRIBUTES /////
 
+
+  // ATTRIBUTES /////
+  private File directoryBSUDatabase;
+  private File directoryBudgets;
+  private File directoryExpenses;
+  private File fileCompleteExpenses;
+
+  // INITIALIZATION BLOCK /////
+  {
+    this.directoryBSUDatabase = new File("./BSUDatabase/");
+    this.directoryBudgets = new File(this.directoryBSUDatabase, "budgets/");
+    this.directoryExpenses = new File(this.directoryBSUDatabase, "expenses/");
+    this.fileCompleteExpenses = new File(this.directoryExpenses, "completeexpenses.txt");
+  }
+
+  
 
 }
