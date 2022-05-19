af08a9624f3f68d017cfdc6001b37c875917a7b3
Yazid Escudero <yazidescudero0414@gmail.com>
2022-05-18 20:17:41 -0600

    Add FileManager to manage program filesystem

---
+++ src/bsuclasses/files/FileManager.java
@@ -0,0 +1,14 @@
+
+
+/**
+ A FileManager object manages the program's files and folders, and their overall
+ stucture - only one object should be created for this program/system and no more.
+
+ @author Yazid Escudero
+ @version 1.0.0
+*/
+public class FileManager {
+
+
+
+}
