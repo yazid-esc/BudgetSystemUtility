0176e9b188389b8ffe14b460caaefa3c42056c58
Yazid Escudero <yazidescudero0414@gmail.com>
2022-05-19 01:49:38 -0600

    Add attributes, constructors, copyTo(), and toString()

--- src/bsuclasses/budget/Budget.java
+++ src/bsuclasses/budget/Budget.java
@@ -1,6 +1,10 @@
 
 package bsuclasses.budget;
 
+import java.util.ArrayList;
+
+import java.time.YearMonth;
+
 /**
  A {@code Budget} object will represent a budget for a month as specified by the user.
 
@@ -10,6 +14,49 @@ package bsuclasses.budget;
 */
 public class Budget {
 
+  // ATTRIBUTES //////
+  private double startingFunds;
+  private double remainingFunds;
+  private double unallocatedFunds;
+  private ArrayList<BudgetCategory> categories;
+  private YearMonth yearMonth;
 
+  // CONSTRUCTORS /////
+  public Budget(double startingFunds) {
+    this.startingFunds = startingFunds;
+  }
 
+  public Budget(Budget budget) {
+    budget.copyTo(this);
+  }
+
+  // OTHER /////
+  public void copyTo(Budget budget) {
+    budget.startingFunds = this.startingFunds;
+    budget.remainingFunds = this.remainingFunds;
+    budget.unallocatedFunds = this.unallocatedFunds;
+    budget.categories = this.categories;
+    budget.yearMonth = this.yearMonth;
+  }
+
+  public String toString() {
+    StringBuilder contents = new StringBuilder();
+
+    contents.append("------------------------------------------------------------------------------------\n");
+    contents.append("\t" + this.yearMonth + "\n");
+    contents.append("------------------------------------------------------------------------------------\n");
+    contents.append(String.format("%20s\n", "Starting Funds: " + this.startinfFunds));
+    contents.append(String.format("%20s\n", "Remaining Funds: " + this.remainingFunds));
+    contents.append(String.format("%20s\n", "Unallocated Funds: " + this.unallocatedFunds));
+    contents.append("\n");
+
+    int numberOfBudgetCategories = (this.categories).size();
+    for(int index = 0; i < numberOfBudgetCategories; ++index)
+      contents.append((this.categories).get(index).toString() + "\n");
+
+    contents.append("\n\n");
+    contents.append("------------------------------------------------------------------------------------\n");
+
+    return contents.toString();
+  }
 }
