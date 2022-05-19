d71106c2b3bca381f9c828bf675023ad64370313
Yazid Escudero <yazidescudero0414@gmail.com>
2022-05-19 01:24:16 -0600

    Add class BudgetCategory

---
+++ src/bsuclasses/budget/BudgetCategory.java
@@ -0,0 +1,61 @@
+
+package bsuclasses.budget;
+
+import java.util.Objects;
+
+/**
+ This {@code BudgetCategory} object will represent a category in a budget. For example, a June budget might
+ have a "FieldTrip" expense category. Or, a  "Phone Bill" or "Savings" category. A BudgetCategory object has
+ a name and a funds amount (the funds amount is the amount allocated to this specific spending category). For
+ example, a "Bills" spending category might be given $1000 for the user's bills.
+
+ @author Yazid Escudero
+ @version 1.0.0
+ @since 1.0.0
+*/
+public class BudgetCategory {
+
+  // ATTRIBUTES /////
+  private String name;
+  private double funds;
+
+  // CONSTRUCTORS /////
+  public BudgetCategory(String name, double funds) {
+    this.name = name;
+    this.funds = funds;
+  }
+
+  public  BudgetCategory(BudgetCategory budgetCategory) throws NullPointerException{
+    this(budgetCategory.name, budgetCategory.funds);
+  }
+
+  // SETTERS /////
+  /**
+   @param funds the funds to set
+  */
+  public void setFunds(double funds) {
+    this.funds = funds;
+  }
+
+  // GETTERS /////
+  /**
+   @return the name
+  */
+  public void getName() {
+    return this.name;
+  }
+
+  /**
+   @return the funds
+  */
+  public void getFunds() {
+    return this.funds;
+  }
+
+  // OTHER /////
+  @Override
+  public String toString() {
+    String contents = String.format("%35s | %f.2", ("BudgetCategory: " + this.name), this.funds);
+    return contents;
+  }
+}
