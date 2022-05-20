565f081197614b7512962abeb0c4057bd5092b36
Yazid Escudero <yazidescudero0414@gmail.com>
2022-05-19 21:56:32 -0600

    Add class Operations.java

---
+++ src/bsuclasses/operations/Operations.java
@@ -0,0 +1,17 @@
+
+package bsuclases.operations;
+
+/**
+  An object of {@code Operations} class serves as a "center of operations" that sits between the
+  user interface class {@code BudgetApplication} in {@code BudgetApplication.java} and the other
+  program files.
+
+  @author Yazid Escudero
+  @version 1.0.0
+  @since 1.0.0
+*/
+public class Operations {
+
+
+
+}
