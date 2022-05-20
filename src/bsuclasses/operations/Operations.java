2b7da4a02d3ffb0c3fb3aa00daa21adc66419db4
Yazid Escudero <yazidescudero0414@gmail.com>
2022-05-19 22:38:41 -0600

    Add attributes and initialization block

--- src/bsuclasses/operations/Operations.java
+++ src/bsuclasses/operations/Operations.java
@@ -1,6 +1,19 @@
 
 package bsuclases.operations;
 
+import bsuclasses.budget.*;
+import bsuclases.files.*;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.FileNotFounException;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+import java.time.YearMonth;
+
 /**
   An object of {@code Operations} class serves as a "center of operations" that sits between the
   user interface class {@code BudgetApplication} in {@code BudgetApplication.java} and the other
@@ -12,6 +25,42 @@ package bsuclases.operations;
 */
 public class Operations {
 
+  // ATTRIBUTES /////
+  private FileManager fileManager;
+  private ArrayList<Budgets> budgets;
+  private YearMonth now;
+  boolean budgetInitializedForThisMonth;
+
+  // INITIALIZATION BLOCK /////
+  {
+    this.fileManager = new FileManager();
+    this.now = YearMonth.now();
+
+    // Initalize this.budgets
+    {
+      ArrayList<File> filesInBudgets = (this.fileManager).getFilesInBudgetsClone();
+      int size = filesInBudget.size();
+
+      for(int index = 0; index < size; +index)
+        (this.budgets).add(Budgets.readBudgetFromFile(filesInBudgets.get(index)));
+    }
+
+    // Initialize this.budgetInitializedForThisMonth
+    //
+    // If this.budgets contains a budget with a YearMonth object that matches this.now
+    // then the budget for this month is considered initialized
+    {
+      int size = (this.budgets).size();
+      for(int index = 0; index < size; ++index) {
+        if((this.now).equals((this.budgets).get(index).getYerMonth())) {
+          this.budgetInitializedForThisMonth = true;
+          break;
+        }
+      }
+    }
+
+
+  }
 
 
 }
