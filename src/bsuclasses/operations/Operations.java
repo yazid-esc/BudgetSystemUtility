2446e53365d3b8e2cda0f535778085648d5eec15
Yazid Escudero <yazidescudero0414@gmail.com>
2022-05-23 02:45:19 -0600

    Fix typo

--- src/bsuclasses/operations/Operations.java
+++ src/bsuclasses/operations/Operations.java
@@ -39,7 +39,7 @@ public class Operations {
 
     // Verify necessary files and directories needed for proper program behavior
     try {
-      (this.fileManager).verifiyFileSystem();
+      (this.fileManager).verifyFileSystem();
     } catch(IOException ioe) {
       System.out.println("IOExceptoin happend when verifying program's filesystem - FATAL\n\n");
       System.out.println(ioe.getMessage() + "\n");
